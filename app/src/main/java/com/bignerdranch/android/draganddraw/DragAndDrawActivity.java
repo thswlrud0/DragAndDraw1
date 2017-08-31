7654763f1619b0b2c33451fb63cce539a043b9b1
thsjikyeong <thswlrud0@naver.com>
2017-08-31 22:03:00 +0900

    test commit and push //  enable 로 해보기 왜안돼

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class DragAndDrawActivity extends SingleFragmentActivity {
     //git add test
-    //왜안돼
+    //왜안돼ㅁㅁ
     @Override
     protected Fragment createFragment(){
         return DragAndDrawFragment.newInstance();
